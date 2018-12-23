5d4f0eeb429693bc1dcef39fa5d117a125abbb7b
risingone <anuragdhingra101@gmail.com>
2018-12-24 05:46:24 +0900

    adding multiplexer, initial server config

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
