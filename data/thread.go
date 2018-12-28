82f1669e52101be8e86c3b111383a299651c3bd8
risingone <anuragdhingra101@gmail.com>
2018-12-28 18:32:53 +0900

    Added httprouter for getting particular thread

--- data/thread.go
+++ data/thread.go
@@ -2,15 +2,16 @@ package data
 
 import (
 	"log"
+	"strconv"
 	"time"
 )
 
 type Thread struct {
-	Id int
-	Uuid string
-	Topic string
-	UserId int
-	CreatedAt time.Time
+	Id int `json:"id"`
+	Uuid string `json:"uuid"`
+	Topic string `json:"topic"`
+	UserId int `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func Threads() (threads []Thread, err error) {
@@ -30,5 +31,22 @@ func Threads() (threads []Thread, err error) {
 		threads = append(threads, conv)
 	}
 	rows.Close()
+	return
+}
+
+func ThreadByID(threadId string) (thread Thread, err error) {
+	id, err := strconv.Atoi(threadId)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	conv := Thread{}
+	err = Db.QueryRow("SELECT * FROM threads WHERE id = ?",id).Scan(&conv.Id, &conv.Uuid, &conv.Topic, &conv.UserId, &conv.CreatedAt)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	thread = conv
 	return
 }
--- route_main.go
+++ route_main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"chit-chat/data"
+	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	threads, err := data.Threads()
 	if err != nil {
 		log.Print(err)
 		return
 	} else {
-		log.Print("index fired")
+		generateHTML(w, threads, "layout", "public.navbar", "index")
+		}
+	}
+
+func FindThread(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	threadId := p.ByName("id")
+	thread, err := data.ThreadByID(threadId)
+	threads := []data.Thread{}
+	threads = append(threads, thread)
+	if err != nil {
+		log.Print(err)
+		return
+	} else {
 		generateHTML(w, threads, "layout", "public.navbar", "index")
 	}
 }
