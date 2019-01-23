089bae653481636acf673cfe926c571645162a0a
anuragdhingra <anuragdhingra101@gmail.com>
2019-01-23 21:57:27 +0900

    Correcting typo in deploy-heroku.sh

--- infra/deploy-heroku.sh
+++ infra/deploy-heroku.sh
@@ -2,7 +2,7 @@
 
 set -e
 set -u
-source ../setup-env.sh
+source ./setup-env.sh
 echo "Pushing branch ${CIRCLE_BRANCH} to app ${HEROKU_APP}"
 git remote add heroku https://git.heroku.com/${HEROKU_APP}.git
 git push heroku ${CIRCLE_BRANCH}:master
